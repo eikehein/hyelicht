1ecd2eb80e7c03f6c67ea43735c616f4fc7efb38
Eike Hein <sho@eikehein.com>
2025-06-16 22:57:43 +0200

    Port to new Home Assistant API
    The old API was deprecated and removed in v2025.6.

--- support/homeassistant/hyelicht/__init__.py
+++ support/homeassistant/hyelicht/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-# SPDX-FileCopyrightText: 2024 Eike Hein <sho@eikehein.com>
+# SPDX-FileCopyrightText: 2024-2025 Eike Hein <sho@eikehein.com>
 
 """The Hyelicht integration."""
 
@@ -7,12 +7,15 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
 from .const import DOMAIN
 
+PLATFORMS: list[Platform] = [Platform.LIGHT]
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setup(entry, "light")
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    return await hass.config_entries.async_forward_entry_unload(entry, "light")
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
